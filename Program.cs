728a8f1817feba806a156148b0bfcc261ccba6c2
txmn <toximan@yandex.ru>
2021-10-18 20:02:58 +0300

    inventory task

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleInventory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Item> tableItems = new List<Item>() { new Item("Amulet", 'V'), new Item("Coin", '@', 33), new Item("Book", 'B') };
+            List<Item> drawerItems = new List<Item>() { new Item("Item", 'I', 14), new Item("Map", 'M', 88) };
+
+            Storage table = new Storage("Table", 10, tableItems, 20, 10);
+            Storage drawer = new Storage("Drawer", 10, drawerItems, 30, 15);
+
+            Room kitchen = new Room("Kitchen", new List<Storage>() { table, drawer });
+            kitchen.Show();
+        }
+    }
+
+    public static class Player
+    {
+        public static List<Item> inventoryItems = new List<Item>() { new Item("Key", 'K', 2), new Item("Card", 'C', 3) };
+        public static Storage inventory = new Storage("Inventory", 10, inventoryItems, 40, 5);
+    }
+
+    public class Room
+    {
+        public string Name { get; private set; }
+        public List<Storage> Furniture { get; private set; }
+        public Room(string name, List<Storage> furniture)
+        {
+            Name = name;
+            Furniture = furniture;
+        }
+        public void Show()
+        {
+            Player.inventory.DrawContent(false);
+            Player.inventory.LookInside();
+
+            string output = "";
+            Console.WriteLine($"You are in {Name} room. You see the following:");
+            for (int i = 0; i < Furniture.Count; i++)
+            {
+                output += $"{i + 1}. {Furniture[i].Name}\n";
+            }
+            Console.WriteLine(output);
+
+            string input = Console.ReadLine();
+            Regex onlyDigitsRegEx = new Regex(@"\D");
+            if (onlyDigitsRegEx.IsMatch(input) || input.Equals(""))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Only digits allowed.\n");
+                Show();
+                return;
+            }
+            else
+            {
+                int inputInt = Int32.Parse(input);
+                if (inputInt - 1 >= Furniture.Count || inputInt <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Out of range. Try again.\n");
+                    Show();
+                    return;
+                }
+                Console.Clear();
+                Furniture[inputInt - 1].Interact();
+                Show();
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; private set; }
+        public int Amount { get; private set; }
+        public char Symbol { get; private set; }
+
+        public Item(string itemName, char symbol, int itemAmount = 1)
+        {
+            Name = itemName;
+            Symbol = symbol;
+            Amount = itemAmount;
+        }
+
+        public override string ToString()
+        {
+            return $"Item: {Name}({Symbol}), Amount: {Amount}";
+        }
+    }
+}
---
+++ Storage.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleInventory
+{
+    public class Storage
+    {
+        public string Name { get; private set; }
+        public int Capacity { get; protected set; }
+        public List<Item> Items { get; protected set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Storage(string name, int capacity, List<Item> items, int w, int h)
+        {
+            Name = name;
+            Capacity = capacity;
+            Items = new List<Item>(capacity);
+            Width = w;
+            Height = h;
+            AddItems(items);
+            UpdateCapacity();
+        }
+
+        public void AddItem(Item item)
+        {
+            if (Items == null)
+            {
+                Items = new List<Item>(Capacity);
+            }
+            if (Items.Count <= Capacity)
+            {
+                Items.Add(item);
+                UpdateCapacity();
+            }
+            else
+            {
+                Console.WriteLine("Your inventory is full.");
+            }
+        }
+
+        private void AddItems(List<Item> items)
+        {
+            foreach (Item item in items)
+            {
+                AddItem(item);
+            }
+        }
+
+        private void RemoveItem(Item item)
+        {
+            if (Items != null && Items.Count > 0 && Items.Contains(item))
+            {
+                Items.Remove(item);
+                UpdateCapacity();
+            }
+        }
+
+        private void RemoveItems(List<Item> items)
+        {
+            foreach (Item item in items)
+            {
+                RemoveItem(item);
+            }
+        }
+
+        private void UpdateCapacity()
+        {
+            Capacity = Items.Count;
+        }
+
+        public void DrawContent(bool isInteractable)
+        {
+            string drawString = "";
+            for (int i = 0; i < Width; i++)
+            {
+                drawString += "_";
+            }
+
+            for (int j = 0; j < Height; j++)
+            {
+                drawString += "\n";
+                drawString += "|";
+                for (int i = 0; i < Width; i++)
+                {
+                    drawString += " ";
+                }
+                drawString += "|";
+            }
+            drawString += "\n";
+
+            for (int k = 0; k < Width; k++)
+            {
+                drawString += "_";
+            }
+
+            drawString = DrawItemsInside(drawString, Items);
+            Console.WriteLine(drawString);
+            if (isInteractable)
+            {
+                LookInside();
+                Console.WriteLine("0. Back.");
+            }
+        }
+
+        private string DrawItemsInside(string furnitureString, List<Item> items)
+        {
+            Random rnd = new Random();
+            foreach (Item item in items)
+            {
+                int rndIndex = rnd.Next(furnitureString.Length - 1);
+                while (!furnitureString[rndIndex].Equals(' '))
+                {
+                    rndIndex = rnd.Next(furnitureString.Length - 1);
+                }
+                furnitureString = furnitureString.Remove(rndIndex, 1);
+                furnitureString = furnitureString.Insert(rndIndex, item.Symbol.ToString());
+            }
+            return furnitureString;
+        }
+
+        public void Interact()
+        {
+            Console.WriteLine($"You look inside a {Name}.");
+            DrawContent(true);
+
+            string input = Console.ReadLine();
+            Regex onlyDigitsRegEx = new Regex(@"\D");
+            if (onlyDigitsRegEx.IsMatch(input) || input.Equals(""))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Only digits allowed.\n");
+                Interact();
+                return;
+            }
+            else
+            {
+                int inputInt = Int32.Parse(input);
+                if (inputInt - 1 >= Items.Count || inputInt < 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Out of range. Try again.\n");
+                    Interact();
+                    return;
+                }
+                else if(inputInt == 0)
+                {
+                    Console.Clear();
+                    return;
+                }
+                Console.Clear();
+
+                Player.inventory.AddItem(Items[inputInt - 1]);
+                RemoveItem(Items[inputInt - 1]);
+
+                Interact();
+            }
+        }
+
+        public void LookInside()
+        {
+            string itemsString = "";
+            string itemsMessage = "";
+            if (Items == null || Items.Count == 0)
+            {
+                itemsMessage = $"You see an empty {Name}.";
+            }
+            else
+            {
+                itemsMessage = $"You see several items on the {Name}.";
+                foreach (Item item in Items)
+                {
+                    int listIndex = Items.IndexOf(item) + 1;
+                    itemsString += $"{listIndex}. {item.ToString()}.\n";
+                }
+            }
+            Console.WriteLine(itemsMessage);
+            Console.WriteLine(itemsString);
+        }
+    }
+}
